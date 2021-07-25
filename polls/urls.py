34e4a01daa9657436343ff14aa96398cdf3a5b2b
udhaykumar <udhaykumar@udhaykumars-MacBook-Pro.local>
2021-07-25 13:25:06 +0530

    poll completed with result page

--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('start', views.start, name='start'),
+    path('<int:question_id>/', views.details, name='details'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
